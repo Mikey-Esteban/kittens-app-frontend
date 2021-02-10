a59b5e6f876b6afde82960d5e144e431f1447c40
Mikey Esteban <michaelp.esteban@gmail.com>
2021-02-10 10:06:24 -0500

    moved Kittens useState useEffect into Kittens component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,11 @@
-import React, { useState, useEffect, Fragment } from 'react'
-import axios from 'axios'
+import React, { Fragment } from 'react'
 import Kittens from './Kittens/Kittens'
 
 const Home = () => {
 
-  const [ kittens, setKittens ] = useState([])
-
-  useEffect( () => {
-    axios.get('http://localhost:3000/api/v1/kittens')
-      .then( resp => {
-        setKittens(resp.data.data)
-      })
-      .catch( resp => console.log(resp) )
-  }, [kittens.length])
-
-
   return (
     <Fragment>
-      <Kittens kittens={kittens} />
+      <Kittens />
     </Fragment>
   )
 }
